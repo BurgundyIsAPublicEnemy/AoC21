9480a4ba69ef46e05483d67dfeafa363f16f0bba
Vikram <veryseriousemailaddress@gmail.com>
2021-12-08 22:13:28 +0000

    Day 8

--- Day_8/Day8.py
+++ Day_8/Day8.py
@@ -1,6 +1,6 @@
 import itertools
 
-# SHOUT OUT TO https://github.com/julian-west/adventofcode/blob/master/2021/day_08/d8_solution.py
+# SHOUT OUT TO https://github.com/julian-west/adventofcode/blob/master/2021/day_08/d8_solution.py for the help!
 # Attempt 1: Get lengths (easy enough), felt I was gonna permuate cause it felt small but code became spagetthi and logic was rough
 # What did I learn? List Comprehension, For Each
 
